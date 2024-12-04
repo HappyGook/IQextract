adcd1d8f83dfe9fe59bc0f07b9f510e55011454c
oleg <olegradzhabov@gmail.com>
2024-12-04 14:31:10 +0100

    all funcs in main

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
@@ -28,6 +29,54 @@ const (
 	AirspyVID = gousb.ID(0x1D50)
 	AirspyPID = gousb.ID(0x60A1)
 )
+
+// ExtractIQData WAV -> IQ
+func ExtractIQData(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+
+	// Parse WAV header (assumes a fixed 44-byte header for simplicity)
+	header := make([]byte, 44)
+	_, err = file.Read(header)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract raw IQ data
+	var data []int16
+	for {
+		var sample int16
+		err := binary.Read(file, binary.LittleEndian, &sample)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, sample)
+	}
+
+	dataByte := make([]byte, len(data)*2)
+	for i, sample := range data {
+		binary.LittleEndian.PutUint16(dataByte[i*2:], uint16(sample))
+	}
+
+	return dataByte, nil
+}
+
+// SendIQData IQ geschickt
+func SendIQData(device *gousb.OutEndpoint, data []byte) error {
+	_, err := device.Write(data)
+	return err
+}
 
 func extractHandler(c *gin.Context) {
 
